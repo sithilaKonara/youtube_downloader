7a10b27b045d42d8319a030a7291b16c2491554b
unknown <sithila@yahoo.com>
2023-10-16 13:16:29 +0300

    terraform connection test

--- provider.tf
+++ provider.tf
@@ -5,11 +5,12 @@ terraform {
       version = "5.21.0"
     }
   }
-  backend "remote" {
-    organization = "sk-tf-cloud"
-    workspaces {
-      name = "youtube_downloader"
-    }
+  backend "s3" {
+    bucket         = "sk-terraform-tfstate-bkt"
+    key            = "youtubeDownloader/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "sk-terraform-state-locking"
+    encrypt        = true
   }
 }
 
