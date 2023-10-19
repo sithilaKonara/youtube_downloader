e37a5b1ffe4b1757ca85739cbf207ae5a47b5f19
unknown <sithila@yahoo.com>
2023-10-19 22:02:39 +0300

    lambda layer test

--- backend/modules/api_backend/lambda.tf
+++ backend/modules/api_backend/lambda.tf
@@ -1,11 +1,11 @@
 data "archive_file" "d_pytube_layer" {
   type        = "zip"
-  source_dir = "${path.root}/modules/api_backend/documents/lambda/pytube/venv/Lib/site-packages"
-  output_path = "${path.root}/modules/api_backend/documents/lambda.layer.zip"
+  source_dir = "${path.root}/modules/api_backend/documents/lambda/pytube/layer/"
+  output_path = "${path.root}/modules/api_backend/documents/lambda_layer.zip"
 }
 
 resource "aws_lambda_layer_version" "r_pytube_layer" {
-  filename   = "${path.root}/modules/api_backend/documents/lambda.layer.zip"
+  filename   = "${path.root}/modules/api_backend/documents/lambda_layer.zip"
   layer_name = "pytube_core"
 
   compatible_runtimes = ["python3.11"]
