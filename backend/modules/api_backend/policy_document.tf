2b93735a96c276c3e632187989d4775b977a7175
unknown <sithila@yahoo.com>
2023-10-19 23:28:36 +0300

    add new lambda function 01

--- backend/modules/api_backend/policy_document.tf
+++ backend/modules/api_backend/policy_document.tf
@@ -1,8 +1,8 @@
 data "aws_iam_policy_document" "d_get_ytd_object" {
   statement {
     effect = "Allow"
-    actions = "logs:CreateLogGroup"
-    resources = "arn:aws:logs:${var.v_backend_region_id}:${var.v_backend_account_id}:*"
+    actions = ["logs:CreateLogGroup"]
+    resources = ["arn:aws:logs:${var.v_backend_region_id}:${var.v_backend_account_id}:*"]
   }
   statement {
     effect = "Allow"
@@ -10,7 +10,7 @@ data "aws_iam_policy_document" "d_get_ytd_object" {
         "logs:CreateLogStream",
         "logs:PutLogEvents"
     ]
-    resources = "arn:aws:logs:${var.v_backend_region_id}:${var.v_backend_account_id}:log-group:/aws/lambda/get_ytd_object:*"
+    resources = ["arn:aws:logs:${var.v_backend_region_id}:${var.v_backend_account_id}:log-group:/aws/lambda/get_ytd_object:*"]
   }
 }
 
