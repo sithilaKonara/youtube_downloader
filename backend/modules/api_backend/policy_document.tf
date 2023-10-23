b941ae85783ec40610b97fe2b1a174e457286343
unknown <sithila@yahoo.com>
2023-10-23 10:58:28 +0300

    add download_ytd_object lambda

--- backend/modules/api_backend/policy_document.tf
+++ backend/modules/api_backend/policy_document.tf
@@ -14,6 +14,22 @@ data "aws_iam_policy_document" "d_get_ytd_object" {
   }
 }
 
+data "aws_iam_policy_document" "d_download_ytd_object" {
+  statement {
+    effect = "Allow"
+    actions = ["logs:CreateLogGroup"]
+    resources = ["arn:aws:logs:${var.v_backend_region_id}:${var.v_backend_account_id}:*"]
+  }
+  statement {
+    effect = "Allow"
+    actions = [
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+    ]
+    resources = ["arn:aws:logs:${var.v_backend_region_id}:${var.v_backend_account_id}:log-group:/aws/lambda/download_ytd_object:*"]
+  }
+}
+
 
 # Lambda assume role policy
 data "aws_iam_policy_document" "d_lambda_assume_role" {
