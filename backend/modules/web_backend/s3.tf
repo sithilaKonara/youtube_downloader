2e27e6b740a30940e30d1f626d9eee2a7f3c8a7b
unknown <sithila@yahoo.com>
2023-10-23 20:50:23 +0300

    add cloudfront annd config

--- backend/modules/web_backend/s3.tf
+++ backend/modules/web_backend/s3.tf
@@ -6,3 +6,16 @@ resource "aws_s3_bucket_acl" "r_bkt_acl" {
   bucket = aws_s3_bucket.r_ytd_web_s3.id
   acl    = "private"
 }
+
+# create S3 website hosting:
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.r_ytd_web_s3.id
+  index_document {
+    suffix = "index.html"
+  }
+}
+# add bucket policy to let the CloudFront OAI get objects:
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.r_ytd_web_s3.id
+  policy = data.aws_iam_policy_document.d_bucket_policy_document.json
+}
