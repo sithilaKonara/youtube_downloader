d93bdf3a8a94d5938c351392b9b9e32eb7af8497
unknown <sithila@yahoo.com>
2023-10-23 22:42:24 +0300

    add cloudfront annd config

--- backend/modules/web_backend/s3.tf
+++ backend/modules/web_backend/s3.tf
@@ -2,11 +2,6 @@ resource "aws_s3_bucket" "r_ytd_web_s3" {
   bucket = "youtube-downloader-web"
 }
 
-resource "aws_s3_bucket_acl" "r_bkt_acl" {
-  bucket = aws_s3_bucket.r_ytd_web_s3.id
-  acl    = "private"
-}
-
 # create S3 website hosting:
 resource "aws_s3_bucket_website_configuration" "website" {
   bucket = aws_s3_bucket.r_ytd_web_s3.id
