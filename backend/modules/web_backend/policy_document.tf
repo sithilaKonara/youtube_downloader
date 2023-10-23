c1915c4800b9d067c55c0732337ea22ad9544a7b
unknown <sithila@yahoo.com>
2023-10-23 22:20:49 +0300

    add cloudfront annd config

--- backend/modules/web_backend/policy_document.tf
+++ backend/modules/web_backend/policy_document.tf
@@ -3,9 +3,15 @@ data "aws_iam_policy_document" "d_bucket_policy_document" {
   statement {
     effect = "Allow"
     actions = ["s3:GetObject"]
-    resources = [
-      aws_s3_bucket.r_ytd_web_s3.arn,
-      "${aws_s3_bucket.r_ytd_web_s3.arn}/*"
-    ]
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+    resources = ["${aws_s3_bucket.r_ytd_web_s3.arn}/*"]
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = ["arn:aws:cloudfront::554353511998:distribution/55555555552"]
+    }
   }
 }
