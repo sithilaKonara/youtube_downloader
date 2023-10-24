77ec210fadd9092924444773670fb3b2d247a8e5
unknown <sithila@yahoo.com>
2023-10-24 14:34:38 +0300

    add apigateway

--- backend/modules/api_backend/api_gateway.tf
+++ backend/modules/api_backend/api_gateway.tf
@@ -143,7 +143,7 @@ resource "aws_lambda_permission" "r_api_gateway_get" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.r_get_ytd_object.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "arn:aws:execute-api:${var.v_backend_region_id}:${var.v_backend_account_id}:${aws_api_gateway_rest_api.r_ytd_api_gateway.id}/*/${aws_api_gateway_method.r_get.http_method}/${aws_api_gateway_resource.r_ytd_root.path}"
+  source_arn    = "arn:aws:execute-api:${var.v_backend_region_id}:${var.v_backend_account_id}:${aws_api_gateway_rest_api.r_ytd_api_gateway.id}/*/${aws_api_gateway_method.r_get.http_method}${aws_api_gateway_resource.r_ytd_root.path}"
   depends_on = [ 
     aws_api_gateway_integration.r_get_lambda,
     aws_api_gateway_stage.r_api_gw_stage
@@ -155,7 +155,7 @@ resource "aws_lambda_permission" "r_api_gateway_post" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.r_download_ytd_object.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "arn:aws:execute-api:${var.v_backend_region_id}:${var.v_backend_account_id}:${aws_api_gateway_rest_api.r_ytd_api_gateway.id}/*/${aws_api_gateway_method.r_post.http_method}/${aws_api_gateway_resource.r_ytd_root.path}"
+  source_arn    = "arn:aws:execute-api:${var.v_backend_region_id}:${var.v_backend_account_id}:${aws_api_gateway_rest_api.r_ytd_api_gateway.id}/*/${aws_api_gateway_method.r_post.http_method}${aws_api_gateway_resource.r_ytd_root.path}"
   #arn:aws:execute-api:us-east-1:554353511998:ia29poyu60/*/DELETE/example
   depends_on = [ 
     aws_api_gateway_integration.r_post_lambda,
